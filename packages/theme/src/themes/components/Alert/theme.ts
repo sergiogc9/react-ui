ab344ac5cea161dbafcae8b87f642fa0b2a570c3
Sergio G Cosgaya <sergiogcosgaya@gmail.com>
2022-03-12 13:47:37 +0100

    Added dark mode to Alert component

--- packages/theme/src/themes/components/Alert/theme.ts
+++ packages/theme/src/themes/components/Alert/theme.ts
@@ -5,7 +5,21 @@ const alertTheme: Alert = {
 		error: { bg: 'red.100', icon: 'red.700', text: 'red.800' },
 		info: { bg: 'blue.50', icon: 'blue.800', text: 'blue.800' },
 		success: { bg: 'green.100', icon: 'green.900', text: 'green.900' },
-		warning: { bg: 'yellow.50', icon: 'yellow.900', text: 'neutral.900' }
+		warning: { bg: 'yellow.50', icon: 'yellow.900', text: 'neutral.900' },
+		modes: {
+			light: {
+				error: { bg: 'red.100', icon: 'red.700', text: 'red.800' },
+				info: { bg: 'blue.50', icon: 'blue.800', text: 'blue.800' },
+				success: { bg: 'green.100', icon: 'green.900', text: 'green.900' },
+				warning: { bg: 'yellow.50', icon: 'yellow.900', text: 'neutral.900' }
+			},
+			dark: {
+				error: { bg: 'red.600', icon: 'red.100', text: 'red.100' },
+				info: { bg: 'blue.600', icon: 'blue.100', text: 'blue.100' },
+				success: { bg: 'green.600', icon: 'green.100', text: 'green.100' },
+				warning: { bg: 'yellow.600', icon: 'yellow.50', text: 'neutral.50' }
+			}
+		}
 	}
 };
 
--- packages/theme/src/themes/components/Alert/types.ts
+++ packages/theme/src/themes/components/Alert/types.ts
@@ -1,7 +1,9 @@
+import { ThemeColors } from 'theme/types';
+
 type AlertStatus = 'error' | 'info' | 'success' | 'warning';
 
 type AlertColors = Record<AlertStatus, Record<'bg' | 'icon' | 'text', string>>;
 
 export interface Alert {
-	readonly colors: AlertColors;
+	readonly colors: ThemeColors<AlertColors>;
 }
