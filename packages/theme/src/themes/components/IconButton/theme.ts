1669ac582a1731fac9c42acbdbd56a0541392936
Sergio G Cosgaya <sergiogcosgaya@gmail.com>
2022-03-12 15:26:36 +0100

    Added dark mode for IconButton component

--- packages/theme/src/themes/components/IconButton/theme.ts
+++ packages/theme/src/themes/components/IconButton/theme.ts
@@ -1,13 +1,39 @@
-import { IconButton } from './types';
+import cloneDeep from 'lodash/cloneDeep';
+import merge from 'lodash/merge';
+
+import { RecursivePartial } from 'theme/global.types';
+
+import { IconButton, IconButtonColors } from './types';
+
+const lightColors: IconButtonColors = {
+	borderActive: 'primary.400',
+	icon: 'primary.500',
+	background: {
+		default: 'transparent',
+		hover: 'neutral.50',
+		active: 'neutral.100'
+	}
+};
+
+const darkColors: IconButtonColors = merge<IconButtonColors, RecursivePartial<IconButtonColors>>(
+	cloneDeep(lightColors),
+	{
+		borderActive: 'primary.400',
+		icon: 'primary.500',
+		background: {
+			default: 'transparent',
+			hover: 'neutral.700',
+			active: 'neutral.800'
+		}
+	}
+);
 
 const iconButtonTheme: IconButton = {
 	colors: {
-		borderActive: 'primary.200',
-		icon: 'neutral.800',
-		background: {
-			default: 'transparent',
-			hover: 'neutral.50',
-			active: 'neutral.100'
+		...lightColors,
+		modes: {
+			light: lightColors,
+			dark: darkColors
 		}
 	},
 	sizes: {
