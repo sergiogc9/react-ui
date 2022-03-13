e4ec729770c6dd2ae1596c6f03cc10119c2d179e
Sergio G Cosgaya <sergiogcosgaya@gmail.com>
2022-03-13 15:54:25 +0100

    Added dark mode colors for Switch component

--- packages/theme/src/themes/components/Switch/types.ts
+++ packages/theme/src/themes/components/Switch/types.ts
@@ -1,10 +1,16 @@
+import { ThemeColors } from 'theme/types';
+
 type SwitchAspectSizes = 's' | 'm';
 type SwitchSizesParams = Record<'width' | 'height', string>;
 type SwitchBackgroundSizes = Record<SwitchAspectSizes, SwitchSizesParams>;
+type SwitchColors = {
+	background: Record<'checked' | 'default' | 'disabled', string>;
+	toggle: Record<'color', string>;
+};
 type SwitchToggleSizes = Record<SwitchAspectSizes, SwitchSizesParams>;
 
 export interface Switch {
 	readonly backgroundSizes: SwitchBackgroundSizes;
-	readonly color: string;
+	readonly colors: ThemeColors<SwitchColors>;
 	readonly toggleSizes: SwitchToggleSizes;
 }
