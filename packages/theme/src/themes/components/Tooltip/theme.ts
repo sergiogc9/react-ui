878f06416160810e25061201884a7a90eb319204
Sergio G Cosgaya <sergiogcosgaya@gmail.com>
2022-03-13 15:21:18 +0100

    Improved tooltip colors

--- packages/theme/src/themes/components/Tooltip/theme.ts
+++ packages/theme/src/themes/components/Tooltip/theme.ts
@@ -3,12 +3,12 @@ import { Tooltip } from './types';
 const tooltipTheme: Tooltip = {
 	colors: {
 		dark: {
-			background: 'neutral.900',
+			background: 'neutral.700',
 			text: 'neutral.0'
 		},
 		light: {
 			background: 'neutral.100',
-			text: 'neutral.800'
+			text: 'neutral.700'
 		}
 	}
 };
