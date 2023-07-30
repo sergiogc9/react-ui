72c4f0610acc45d2c78f30138ae1b50e71fd77fa
Sergio G Cosgaya <sergiogcosgaya@gmail.com>
2023-07-30 17:32:16 +0200

    fix: Improve rowProps prop in TableContent

--- packages/components/src/Table/Content/types.ts
+++ packages/components/src/Table/Content/types.ts
@@ -4,13 +4,13 @@ import { FlexProps } from 'components/Flex';
 
 import { TableBodyRowProps } from '../Body/Row/types';
 
-export interface TableContentProps extends FlexProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
+export interface TableContentProps<T = any> extends FlexProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
 	/**
 	 * A function to customize the row styles
 	 * @param row: The Row to customize
 	 * @returns An object containing the props for the TableContentRow. Use the `css` prop to use more customizable styles with css() from styled-components or styled-system.
 	 */
-	rowProps?: <T>(row: Row<T>) => Omit<TableBodyRowProps, 'cursor' | 'onClick' | 'showAllBorders'>;
+	rowProps?: (row: Row<T>) => Omit<TableBodyRowProps, 'cursor' | 'onClick' | 'showAllBorders'>;
 	/**
 	 * Show or hide the borders between rows.
 	 */
